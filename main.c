24529ccb65d6d3a426610e19a9a076b32eaffafb
asteriskRonit <ronitpaul3280@gmail.com>
2021-01-25 15:51:19 +0530

    CIG

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void createSymbol(int range, char symbol)
+{
+    for (; range >= 1; range--)
+    {
+        printf("%c ", symbol);
+    }
+}
 
 void main()
 {
-    printf("Hello World");
+    printf("\n");
+    for (int count = 1; count <= 6; count++)
+    {
+        printf("\e[97;1;196m");
+        createSymbol(15, '*');
+        printf("\e[93;5;196m");
+        createSymbol(4, '*');
+        printf("\n");
+    }
+    printf("\n");
 }
